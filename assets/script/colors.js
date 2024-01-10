7c413020dfabd0caab5291a87c261ee6a55288b4
code-front-braga <programer_frontbraga@outlook.com>
2024-01-10 19:51:32 -0300

    Initial commit

---
+++ assets/script/colors.js
@@ -0,0 +1,11 @@
+const colorGreenWhenPaused = () => {
+  return (watch.style.color = 'green');
+};
+
+const colorRedWhenReset = () => {
+  return (watch.style.color = 'red');
+};
+
+const colorBlack = () => {
+  return (watch.style.color = 'black');
+};
